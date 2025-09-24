b2f6a2d7e8d01094b92a510a0ae3aeb762db8b5e
johnvictordepaula032-lang <johnvictordepaula032@gmail.com>
2025-09-24 12:19:06 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,12 +2,60 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
+// Este código inicial servGGGe como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+
+
+    char estado,codigo,cidade,Estado,Codigo,Cidade;
+    int população,População;
+    float pib, aréa,Pib,Aréa; 
+
+printf("**CARTA 1**\n\n");
+
+printf("Estado:\n\n");
+scanf ("%s",&estado);
+
+printf("Código:\n\n");
+scanf ("%s", &codigo);
+
+printf("Cidade:\n\n");
+scanf("%s", &cidade);
+
+printf("População:\n\n");
+scanf("%d", &população);
+
+printf("PIB:\n\n");
+scanf("%f", &pib);
+
+printf("Aréa quadrada:\n\n");
+scanf("%f", &aréa);
+
+printf("**CARTA 2**\n\n");
+
+
+printf("Estado:\n\n");
+scanf ("%s",&Estado);
+
+printf("Código:\n\n");
+scanf ("%s", &Codigo);
+
+printf("Cidade:\n\n");
+scanf("%s", &Cidade);
+
+printf("População:\n\n");
+scanf("%d", &População);
+
+printf("PIB:\n\n");
+scanf("%f", &Pib);
+
+
+printf("Aréa quadrada:\n\n");
+scanf("%f", &Aréa);
+
+
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Cadastro das Cartas:
